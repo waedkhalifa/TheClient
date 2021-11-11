58cd7363aa7eac3fd6ad6e0060c7fdfaa11069a9
diala-eng <71458903+diala-eng@users.noreply.github.com>
2021-11-11 21:48:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,62 @@
 from flask import Flask, jsonify
 import requests
+app = Flask(__name__)
+
+i = int(input("enter your option from 1-3 ,1: search id. 2: search topic. 3:ppurchase. : "))
+
+def search(i):
+  while True:
+     if i == 1:
+      option = int(input("enter id"))
+      searchID(option),
+     elif i == 2:
+      option = input("enter id")
+      searchTopic(option),
+     elif i == 3:
+      option = int(input("enter id"))
+      purchase(option),
+     else :
+      print("incorect option" )
+
+     return print(i)
+      
+
+def searchID(id):
+    req=requests.get("http://192.168.100.8:7000/searchID/{}".format(id))
+
+    if req.status_code == 200:
+        result=req.json() #content of json as dictionary
+        return jsonify(result)
+    elif req.status_code == 404:
+        return 'The server has not found anything matching the URI given'
+    else:
+        return 'Status code '+ req.status_code +' indicates to something ERROR!'
+
+
+
+def searchTopic(topic):
+    req=requests.get("http://192.168.100.8:7000/searchTopic/{}".format(topic)) #send to catalog
+
+    if req.status_code == 200:
+        result=req.json() #content of json as dictionary
+        return jsonify(result)
+    elif req.status_code == 404:
+        return 'The server has not found anything matching the URI given'
+    else:
+        return 'Status code '+ req.status_code +' indicates to something ERROR!'
+
+def purchase(id):
+
+    req = requests.get("http://192.168.100.9:9000/purchase/{}".format(id))
+    if req.status_code == 200:
+        result = req.json()  # content of json as dictionary
+        print('beautiful')
+        return jsonify(result)
+    elif req.status_code == 404:
+        return 'The server has not found anything matching the URI given'
+    else:
+        return 'Status code indicates to something ERROR!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6000)
